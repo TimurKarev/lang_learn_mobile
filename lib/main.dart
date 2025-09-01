4fd97457ad0eabb80f4df76cf9c392209d1cdb06
timurkarev <timur.k@glazgo.online>
2025-09-01 13:14:59 +0600

    feat: lock app orientation to portrait mode only

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lang_learn_mobile/main_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MainApp());
 }
