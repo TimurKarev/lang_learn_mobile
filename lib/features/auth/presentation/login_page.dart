8620370d1dd80e5b25be011c0ae7bf4eda01ddb7
timurkarev <timur.k@glazgo.online>
2025-09-23 00:41:07 +0600

    feat: add social login buttons with Google, Facebook, and Apple options

--- lib/features/auth/presentation/login_page.dart
+++ lib/features/auth/presentation/login_page.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:lang_learn_mobile/features/memory_cards/presentation/bloc/auth_bloc.dart';
+import 'package:lang_learn_mobile/ui_kit/buttons/login_with_button.dart';
 import 'package:lang_learn_mobile/ui_kit/text_fields/password_text_field.dart';
 import 'package:lang_learn_mobile/ui_kit/text_fields/tili_text_field.dart';
 
@@ -21,20 +24,32 @@ class LoginPage extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TiliTextField(hintText: 'Email'),
               SizedBox(height: 16),
               PasswordTextField(),
               SizedBox(height: 16),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () => context.read<AuthBloc>().add(
-                    const AuthSignInAnonymouslyEvent(),
-                  ),
-                  child: const Text('Login Anonymously'),
+              ElevatedButton(
+                onPressed: () => context.read<AuthBloc>().add(
+                  const AuthSignInAnonymouslyEvent(),
                 ),
+                child: const Text('Login Anonymously'),
               ),
+              LoginWithButton(
+                variant: LoginWithButtonVariant.google,
+                onPressed: () {},
+              ),
+              LoginWithButton(
+                variant: LoginWithButtonVariant.facebook,
+                onPressed: () {},
+              ),
+              if (Platform.isIOS)
+                LoginWithButton(
+                  variant: LoginWithButtonVariant.apple,
+                  onPressed: () {},
+                ),
             ],
           ),
         ),
---
+++ lib/ui_kit/buttons/login_with_button.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:lang_learn_mobile/core/theme/tili_palette.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+enum LoginWithButtonVariant { google, apple, facebook }
+
+class LoginWithButton extends StatelessWidget {
+  const LoginWithButton({
+    super.key,
+    required this.variant,
+    this.enabled = true,
+    required this.onPressed,
+  });
+
+  final LoginWithButtonVariant variant;
+  final bool enabled;
+  final VoidCallback onPressed;
+
+  String get _text {
+    switch (variant) {
+      case LoginWithButtonVariant.google:
+        return 'Google';
+      case LoginWithButtonVariant.apple:
+        return 'Apple';
+      case LoginWithButtonVariant.facebook:
+        return 'Facebook';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: TiliPalette.white,
+        foregroundColor: TiliPalette.grayDark,
+      ),
+      onPressed: enabled ? () => onPressed.call() : null,
+      icon: Icon(
+        variant == LoginWithButtonVariant.google
+            ? FontAwesomeIcons.google
+            : variant == LoginWithButtonVariant.apple
+            ? FontAwesomeIcons.apple
+            : FontAwesomeIcons.facebook,
+      ),
+      label: Text('Login with $_text'),
+    );
+  }
+}
