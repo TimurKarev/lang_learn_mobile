ba7e7ab65a6e1f7ca2dfcbddf672b93bbcab9e83
timurkarev <timur.k@glazgo.online>
2025-10-31 16:55:29 +0600

    refactor(ui): restructure memory card answer body layout and reposition hint button

--- lib/ui_kit/cards/memory_card/widgets/memory_card_answer_body.dart
+++ lib/ui_kit/cards/memory_card/widgets/memory_card_answer_body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:lang_learn_mobile/core/entities/litera.dart';
-import 'package:lang_learn_mobile/ui_kit/buttons/outline_icon_button.dart';
 import 'package:lang_learn_mobile/ui_kit/cards/memory_card/widgets/add_hint/add_hint_dialog.dart';
 
 class MemoryCardAnswerBody extends StatelessWidget {
@@ -19,109 +18,132 @@ class MemoryCardAnswerBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
-        const Spacer(),
-        Stack(
+        Column(
           children: [
-            Column(
+            const Spacer(),
+            Stack(
               children: [
-                Text(
-                  answer.lang.displayRussianName,
-                  style: Theme.of(context).textTheme.labelMedium?.copyWith(
-                    color: Theme.of(context).colorScheme.outline,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 12),
-                Text(
-                  answer.word,
-                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                    color: Theme.of(context).colorScheme.onSurfaceVariant,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 12),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                Column(
                   children: [
-                    Flexible(
-                      child: Text(
-                        '/${answer.transcript}/',
-                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                          color: Theme.of(context).colorScheme.onSurface,
-                        ),
+                    Text(
+                      answer.lang.displayRussianName,
+                      style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                        color: Theme.of(context).colorScheme.outline,
                       ),
+                      textAlign: TextAlign.center,
                     ),
-                    if (answer.hasAudio)
-                      IconButton(
-                        icon: Icon(
-                          Icons.volume_up,
-                          color: Theme.of(context).colorScheme.tertiary,
+                    const SizedBox(height: 12),
+                    Text(
+                      answer.word,
+                      style: Theme.of(context).textTheme.headlineLarge
+                          ?.copyWith(
+                            color: Theme.of(
+                              context,
+                            ).colorScheme.onSurfaceVariant,
+                          ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Flexible(
+                          child: Text(
+                            '/${answer.transcript}/',
+                            style: Theme.of(context).textTheme.bodyLarge
+                                ?.copyWith(
+                                  color: Theme.of(
+                                    context,
+                                  ).colorScheme.onSurface,
+                                ),
+                          ),
                         ),
-                        onPressed: () => onPressAudio(answer.id),
-                      ),
+                        if (answer.hasAudio)
+                          IconButton(
+                            icon: Icon(
+                              Icons.volume_up,
+                              color: Theme.of(context).colorScheme.tertiary,
+                            ),
+                            onPressed: () => onPressAudio(answer.id),
+                          ),
+                      ],
+                    ),
                   ],
                 ),
               ],
             ),
-            Positioned(
-              top: 0,
-              right: 16,
-              child: OutlineIconButton(
-                icon: Icons.lightbulb,
-                onPressed: () => showAddHintDialog(context, literaId: literaId),
-                foregroundColor: Theme.of(context).colorScheme.primary,
+
+            Divider(
+              thickness: 1,
+              height: 64,
+              indent: 32,
+              endIndent: 32,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            Text(
+              question.lang.displayRussianName,
+              style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                color: Theme.of(context).colorScheme.outline,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              question.word,
+              style: Theme.of(context).textTheme.headlineMedium,
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Text(
+                      '/${question.transcript}/',
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                        color: Theme.of(context).colorScheme.onSurface,
+                      ),
+                    ),
+                  ),
+                  if (question.hasAudio)
+                    IconButton(
+                      icon: Icon(
+                        Icons.volume_up,
+                        color: Theme.of(context).colorScheme.tertiary,
+                      ),
+                      onPressed: () => onPressAudio(question.id),
+                    ),
+                ],
               ),
             ),
+            const Spacer(),
           ],
         ),
 
-        Divider(
-          thickness: 1,
-          height: 64,
-          indent: 32,
-          endIndent: 32,
-          color: Theme.of(context).colorScheme.primary,
-        ),
-        Text(
-          question.lang.displayRussianName,
-          style: Theme.of(context).textTheme.labelMedium?.copyWith(
-            color: Theme.of(context).colorScheme.outline,
-          ),
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: 8),
-        Text(
-          question.word,
-          style: Theme.of(context).textTheme.headlineMedium,
-          textAlign: TextAlign.center,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+        Positioned(
+          top: 16,
+          right: 16,
+          child: Stack(
             children: [
-              Flexible(
-                child: Text(
-                  '/${question.transcript}/',
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                    color: Theme.of(context).colorScheme.onSurface,
-                  ),
+              Positioned(
+                bottom: 6,
+                right: 0,
+                child: Icon(
+                  Icons.add,
+                  color: Theme.of(context).colorScheme.onSurfaceVariant,
                 ),
               ),
-              if (question.hasAudio)
-                IconButton(
-                  icon: Icon(
-                    Icons.volume_up,
-                    color: Theme.of(context).colorScheme.tertiary,
-                  ),
-                  onPressed: () => onPressAudio(question.id),
-                ),
+              IconButton(
+                icon: Icon(Icons.lightbulb_outline_rounded),
+                iconSize: 42,
+                onPressed: () => showAddHintDialog(context, literaId: literaId),
+              ),
             ],
           ),
         ),
-        const Spacer(),
       ],
     );
   }
